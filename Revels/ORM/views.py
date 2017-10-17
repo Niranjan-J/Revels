cc7ec65eec8106a7a2cd2ab4e13093588558a6c9
kanishkarj <kanishkarj@hotmail.com>
2017-10-17 21:23:44 +0530

    tweak

--- Revels/ORM/views.py
+++ Revels/ORM/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from ORM.dbconnect import Connector;
-from ORM.video import Video;
+from ORM.video import VideoManager;
 
 def blah(request):
     conn = Connector()
-    vid = Video()
-    vid.create({"title":"hello","descr":"hello","url":"hello","img":"hello","user_id":1})
-    vid.insert()
+    vid = VideoManager()
+    vid.insert({"title":"helo","descr":"hello","url":"hello","img":"hello","user_id":1})
     return JsonResponse({"token": "dgfsdg"})
