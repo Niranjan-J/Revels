c80c154c2e350f5b07b8bcf14d754bb72e3b36b7
kanishkarj <kanishkarj@hotmail.com>
2017-10-19 16:29:24 +0530

    Added Auth URL's

--- Revels/Auth/views.py
+++ Revels/Auth/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from django.shortcuts import render
+from django.views import View
 # Create your views here.
+
+class SignUp(View):
+    def get(self, request):
+        return render(request,'Auth/signup.html')
