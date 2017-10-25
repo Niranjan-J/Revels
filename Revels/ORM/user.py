efa35066900535ada302502d089e43396c188635
kanishkarj <kanishkarj@hotmail.com>
2017-10-25 19:22:52 +0530

    Merged Auth.User and ORM.User

--- Revels/ORM/user.py
+++ Revels/ORM/user.py
@@ -1,4 +1,8 @@
 from ORM.dbconnect import Connector
+from http.cookies import SimpleCookie
+from ORM.user import User
+from ORM.profile import Profile
+
 
 class User():
 
@@ -8,9 +12,9 @@ class User():
     def createTable(self):
         self.con.query("""
             CREATE TABLE IF NOT EXISTS User (
-            user_id INTEGER AUTO_INCREMENT,
-            username VARCHAR(50) NOT NULL ,
-            email VARCHAR(50) NOT NULL ,
+            user_id INTEGER AUTO_INCREMENT ,
+            username VARCHAR(50) NOT NULL UNIQUE ,
+            email VARCHAR(50) NOT NULL UNIQUE ,
             password VARCHAR(200) NOT NULL ,
 
             PRIMARY KEY (user_id),
@@ -18,7 +22,39 @@ class User():
             );
             """)
 
+    # returns user_id for a user data
+    def getUserId(self,data):
+        us = User()
+        res = self.con.query("""
+            SELECT * FROM `User` WHERE username = %s
+            """,data['username']
+            )
+        return res[0]['user_id']
+
+    #creates entry in USers and PRofile table
     def createUser(self,data):
+
+        #Creates Entry in users table
+        self.insertUser(data)
+
+        # get's the user_id
+        res = self.getUserId(data)
+
+        #Creates Entry in profile table
+        prf = Profile()
+        prf.createProfile(data)
+
+    def signInUser(self,data):
+        res = self.con.query("""
+            SELECT * FROM `User` WHERE username = %s and password = %s
+            """,data['username'],data['password']
+            )
+        if(len(res) == 1) :
+            return True
+        else :
+            return False
+
+    def insertUser(self,data):
         self.con.query("""
             INSERT INTO User(email,username,password) VALUES (%s,%s,%s)
             """,data['email'],data['username'],data['password']
